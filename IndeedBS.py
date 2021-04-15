69f24c76d49ed97c27e4a41a3bc81b06df358ead
Zakiirim <raffdicostanzo77@gmail.com>
2021-04-15 14:56:15 +0200

    Added Wildcards, links and output

--- IndeedBS.py
+++ IndeedBS.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as soup
 import sys
+import pandas as pd
 
 
 #FILTERS
@@ -24,7 +25,6 @@ filters = {
 }
 
 user_input = sys.argv
-print(user_input)
 if len(user_input) < 2:
 	raise Exception("You must provide at least one argument!")
 else:
@@ -35,6 +35,11 @@ else:
 url = 'https://pl.indeed.com/praca?' + "&".join(
 	[x+y.replace(' ', '+') for x, y in filters.items() if y]
 )
+
+output_csv = False if '-O' not in user_input else user_input[user_input.index('-O') + 1]
+if not output_csv:
+	raise Exception("You must provide the -O wildcard with the output name!")
+
 request = requests.get(url)
 response = soup(request.text, 'html.parser')
 pages_count = response.find(id='searchCountPages')
@@ -43,18 +48,27 @@ pagination = response.select("ul[class='pagination-list'] > li")[:-1]
 if pages_count is None:
 	raise Exception("No result found for your job search. Please change your criteria.")
 else:
-	print("Job Title,Company,Description,Published on")
+	def make_hyperlink(value):
+		if len(value) < 256:
+		    url = 'https://pl.indeed.com{}'
+		    return '=HYPERLINK("%s", "%s")' % (url.format(value), 'CLICK HERE')
+
+	df = pd.DataFrame(columns=["Job Title","Company","Description","Published on", "Link"])
 	for page in ['00'] + ['10', '20', '30', '40'][:len(pagination)]:
 		current_page = '&start=' + page
 		url += current_page
 		request = requests.get(url)
 		response = soup(request.text, 'html.parser')
 		jobtitle = response.select("a[data-tn-element='jobTitle']")
+		links = map(lambda x: make_hyperlink(x['href']), jobtitle)
 		company = response.select("span[class='company']")
 		description = response.select("div[class='summary']")
 		published_on = response.select("span[class='date date-a11y']")
 		for el in zip(jobtitle, company, description, published_on):
-			print(','.join(list(map(lambda x: x.text.replace('\n', ''), el))))
+			df.loc[len(df)] = list(map(lambda x: x.text.replace('\n', ''), el)) + [next(links)]
 
 		request.close()
 		url = url[:len(url)-len(current_page)]
+
+	if len(df) > 0:
+		df.to_csv(output_csv, index=False) if output_csv.endswith(".csv") else df.to_excel(output_csv, index=False)
